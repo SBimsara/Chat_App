4824cfb60118bbad176b365fe4745d8e7ea4dcbe
Sithira Bimsara <basnayakesb.20@uom.lk>
2024-11-13 01:23:30 +0530

    implemented chat room entity

---
+++ src/main/java/com/sithirabimsara/chat/chatroom/ChatRoomService.java
@@ -0,0 +1,13 @@
+package com.sithirabimsara.chat.chatroom;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ChatRoomService {
+
+    public Optional<>
+}
